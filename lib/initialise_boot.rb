000c7bfb9f90b27d8af4305149455685bb9db241
Rob Anderson <rob.anderson@paymentcardsolutions.co.uk>
2014-05-08 12:36:12 +0100

    bump rails

--- lib/initialise_boot.rb
+++ lib/initialise_boot.rb
@@ -4,9 +4,9 @@ exit unless Valve.all.empty?
 
 Valve.create :name => 'Veg sprinkler'
 Valve.create :name => 'Lawn sprinkler'
-Valve.create
-Valve.create
-Valve.create
-Valve.create
-Valve.create
+Valve.create :name => 'Polytunnel left'
+Valve.create :name => 'Polytunnel right'
+Valve.create :name => 'Greenhouse'
+Valve.create :name => 'Veg borders'
+Valve.create :name => 'Orchard'
 Valve.create :name => 'Duck pond'
